390359592a194c371aaadb0be417d6743237fd62
Spectrex1912vm <spectrex1912vm@gmail.com>
2026-05-22 14:28:20 +0300

    Fix GitHub Actions Chrome setup

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -3,6 +3,7 @@ package base;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static helpers.Attach.screenshot;
@@ -24,6 +25,13 @@ public class TestBase {
         Configuration.headless = Boolean.parseBoolean(
                 System.getProperty("selenide.headless", "false")
         );
+
+        Configuration.browserCapabilities = new ChromeOptions()
+                .addArguments(
+                        "--no-sandbox",
+                        "--disable-dev-shm-usage",
+                        "--window-size=1920,1080"
+                );
     }
 
     @AfterEach
