e7de4727dfd3d216e9456ac6dae1a91873bfc929
Spectrex1912vm <spectrex1912vm@gmail.com>
2026-05-22 14:20:27 +0300

    Fix CI configuration

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -4,27 +4,33 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
-
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static helpers.Attach.screenshot;
 
-    public class TestBase {
-        @BeforeAll
-        static void setup() {
-            Configuration.baseUrl = "https://kot.cloud";
-            Configuration.browser = "chrome";
-            Configuration.browserSize = "1920x1080";
-            Configuration.pageLoadStrategy = "eager";
+public class TestBase {
 
-            Configuration.headless = Boolean.parseBoolean(
-                    System.getProperty("selenide.headless", "false")
-            );
-        }
+    @BeforeAll
+    static void setup() {
 
-        @AfterEach
-        void tearDown() {
-            screenshot();
-            closeWebDriver();
-        }
+        Configuration.baseUrl = "https://kot.cloud";
+        Configuration.browser = "chrome";
 
+        Configuration.browserSize = "1920x1080";
+
+        Configuration.timeout = 10000;
+
+        Configuration.pageLoadStrategy = "normal";
+
+        Configuration.headless = Boolean.parseBoolean(
+                System.getProperty("selenide.headless", "false")
+        );
     }
+
+    @AfterEach
+    void tearDown() {
+
+        screenshot();
+
+        closeWebDriver();
+    }
+}
