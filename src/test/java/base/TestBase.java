790f4569424acf8d1113119e93958be4a1a3faa7
Spectrex1912vm <spectrex1912vm@gmail.com>
2026-05-22 14:53:30 +0300

    Fix Chrome

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -3,7 +3,6 @@ package base;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static helpers.Attach.screenshot;
@@ -12,24 +11,11 @@ public class TestBase {
 
     @BeforeAll
     static void setup() {
-
-        Configuration.baseUrl = "https://kot.cloud";
         Configuration.browser = "chrome";
-
         Configuration.headless = true;
         Configuration.browserSize = "1920x1080";
-
         Configuration.timeout = 10000;
         Configuration.pageLoadStrategy = "normal";
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments(
-                "--no-sandbox",
-                "--disable-dev-shm-usage",
-                "--remote-allow-origins=*"
-        );
-
-        Configuration.browserCapabilities = options;
     }
 
     @AfterEach
