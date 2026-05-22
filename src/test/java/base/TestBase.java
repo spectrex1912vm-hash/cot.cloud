ff1a7aca5c4ba7ec9f02b564cc888ed1e7bc54ba
Spectrex1912vm <spectrex1912vm@gmail.com>
2026-05-22 14:43:56 +0300

    Fix TestBase

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -3,6 +3,7 @@ package base;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static helpers.Attach.screenshot;
@@ -13,23 +14,27 @@ public class TestBase {
     static void setup() {
 
         Configuration.baseUrl = "https://kot.cloud";
-
         Configuration.browser = "chrome";
 
         Configuration.headless = true;
-
         Configuration.browserSize = "1920x1080";
 
         Configuration.timeout = 10000;
-
         Configuration.pageLoadStrategy = "normal";
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--remote-allow-origins=*"
+        );
+
+        Configuration.browserCapabilities = options;
     }
 
     @AfterEach
     void tearDown() {
-
         screenshot();
-
         closeWebDriver();
     }
 }
