9daab1d35136b3087788cb314441b3f91ac1318a
Spectrex1912vm <spectrex1912vm@gmail.com>
2026-05-22 14:32:36 +0300

    Fix CI

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -3,7 +3,6 @@ package base;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static helpers.Attach.screenshot;
@@ -14,24 +13,16 @@ public class TestBase {
     static void setup() {
 
         Configuration.baseUrl = "https://kot.cloud";
+
         Configuration.browser = "chrome";
+
+        Configuration.headless = true;
 
         Configuration.browserSize = "1920x1080";
 
         Configuration.timeout = 10000;
 
         Configuration.pageLoadStrategy = "normal";
-
-        Configuration.headless = Boolean.parseBoolean(
-                System.getProperty("selenide.headless", "false")
-        );
-
-        Configuration.browserCapabilities = new ChromeOptions()
-                .addArguments(
-                        "--no-sandbox",
-                        "--disable-dev-shm-usage",
-                        "--window-size=1920,1080"
-                );
     }
 
     @AfterEach
